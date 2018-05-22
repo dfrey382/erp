114faab3a9a826a5de0a398081d30a6b7e0bb895
Fredrick Boaz <fredrickboaz@gmail.com>
2018-05-22 11:19:54 +0300

    Update database.php

--- core/config/database.php
+++ core/config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '50.87.153.143'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'evolve1erp'),
+            'username' => env('DB_USERNAME', 'evolve13_magnum'),
+            'password' => env('DB_PASSWORD', 'watoto2012!!!'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
